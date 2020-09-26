ed54f85b877e788ef723849f2d78691ee2c2f8e0
UralGuru <71888215+UralGuru@users.noreply.github.com>
2020-09-26 16:52:16 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GoatInGarden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Длина веревки:  ");
+            string lenghtString = Console.ReadLine();
+            double LenghtDouble = Convert.ToDouble(lenghtString);
+
+            Console.WriteLine("Размеры огорода:  ");
+            string SizeString = Console.ReadLine();
+            double SizeDouble = Convert.ToDouble(SizeString);
+
+            Console.WriteLine(Calculate(LenghtDouble, SizeDouble));
+        }
+
+        static double Calculate(double LenghtDouble, double SizeDouble)
+        {
+            double s;
+            if (LenghtDouble <= SizeDouble / 2)
+            {
+                s = Math.PI * LenghtDouble * LenghtDouble;
+            }
+            else if (LenghtDouble >= SizeDouble * Math.Sqrt(2) / 2)
+            {
+                s = SizeDouble * SizeDouble;
+            }
+            else
+            {
+                var acos = 2 * Math.Acos(SizeDouble / (2 * LenghtDouble));
+                s = LenghtDouble * LenghtDouble * (Math.PI - 2 * ((Math.PI * acos)) / 180 - Math.Sin(acos));
+            }
+
+            return s;
+        }
+    }
+}
